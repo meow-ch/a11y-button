dbe5fd5f12fd139c75e90f959f808b8e4637ee2d
Guillermo Pages <mail@guillermo.at>
2025-02-17 22:20:23 +0100

    feat: better

--- src/lib/components/ui/IconButton.tsx
+++ src/lib/components/ui/IconButton.tsx
@@ -25,23 +25,12 @@ export function IconButton({
   className,
   ...props
 }: IconButtonProps) {
-  const iconVars = {
-    '--a11y-icon-button-gap': `calc(var(--a11y-button-base-font-size) * ${scale * 0.25})`,
-    '--a11y-icon-button-min-width': text 
-      ? `calc(var(--a11y-button-base-font-size) * ${scale * 2.5})` 
-      : `calc(var(--a11y-button-base-font-size) * ${scale * 1.5})`,
-    '--a11y-icon-button-height': `calc(var(--a11y-button-base-font-size) * ${scale * 1.5})`,
-    '--a11y-icon-button-padding': `calc(var(--a11y-button-base-font-size) * ${scale * 0.25}) calc(var(--a11y-button-base-font-size) * ${scale * 0.5})`,
-    '--a11y-icon-button-font-size': `calc(var(--a11y-button-base-font-size) * ${scale * 0.75})`,
-  } as React.CSSProperties;
-
   return (
     <button
       {...props}
       aria-label={label}
       disabled={disabled}
       className={`${styles['a11y-button-button-icon']} ${active ? styles['a11y-button-button-icon-active'] : ''} ${className || ''}`}
-      style={iconVars}
     >
       {icon}
       {text && <span>{text}</span>}
